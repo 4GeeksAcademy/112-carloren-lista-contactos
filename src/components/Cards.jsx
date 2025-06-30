a3d9c78f08ab96d578d2759b0d8b51cac88f78a6
Carlos Lorenzo <carloren.1996@gmail.com>
2025-06-30 11:40:53 +0000

    obtener contactos

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,32 +1,37 @@
-import { useState } from "react";
+export const Cards = ({ usuario, contactList, getContactList }) => {
 
-export const Cards = () => {
+    function deleteContact(id) {
+        fetch('https://playground.4geeks.com/contact/agendas/' + usuario + '/contacts/' + id, { method: "DELETE" })
+            .then((response) => {
+                if (response.status === 204) {
+                    {getContactList}
+                }
+            })
+            .then()
+            .catch()
 
-    const [contactList, setContactList] = useState([
-        { nombre: "Carlos Lorenzo", direccion: "Calle Falsa 123", tlf: "678 912 345", correo: "carloslorenzo@email.com" },
-        { nombre: "Judith Ramírez", direccion: "Calle Verdadera 987", tlf: "987 654 321", correo: "judithramirez@email.com" },
-        { nombre: "Luis Guilarte", direccion: "Calle 4Geeks 567", tlf: "463 385 404", correo: "luisprofe@email.com" }
-    ])
+    }
 
     return (
-        <div className="d-flex flex-column">
+        <div className="d-flex flex-column text-center">
             {contactList.map((item, index) => (
                 <div key={index} className="card my-2" >
-                    < div className="row g-0" >
-                        <div className="col-md-4">
-                            <img style={{ maxHeight: "200px" }} src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" className="img-fluid rounded-circle m-2 " alt="..." />
+                    < div className="d-flex" >
+                        <img style={{ maxHeight: "200px" }} src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" className="img-fluid rounded-circle m-2 " alt="..." />
+                        <div className="card-body text-start">
+                            <h5 className="card-title">{item.name}</h5>
+                            <p className="card-text"><i className="fa-solid fa-location-dot"></i>&nbsp;&nbsp;&nbsp;{item.address}</p>
+                            <p className="card-text"><i className="fa-solid fa-phone"></i>&nbsp;&nbsp;&nbsp;{item.phone}</p>
+                            <p className="card-text"><i className="fa-solid fa-envelope"></i>&nbsp;&nbsp;&nbsp;{item.email}</p>
                         </div>
-                        <div className="col-md-8">
-                            <div className="card-body text-start">
-                                <h5 className="card-title">{item.nombre}</h5>
-                                <p className="card-text"><i className="fa-solid fa-location-dot"></i>&nbsp;&nbsp;&nbsp;{item.direccion}</p>
-                                <p className="card-text"><i className="fa-solid fa-phone"></i>&nbsp;&nbsp;&nbsp;{item.tlf}</p>
-                                <p className="card-text"><i className="fa-solid fa-envelope"></i>&nbsp;&nbsp;&nbsp;{item.correo}</p>
-                            </div>
+                        <div className="card-body text-end">
+                            <p><i className="crossButton btn fa-solid fa-pencil"></i></p>
+                            <p><i className="crossButton btn fa-solid fa-trash" onClick={() => deleteContact(item.id)}></i></p>
                         </div>
                     </div >
                 </div >
-            ))}
-        </div>
+            ))
+            }
+        </div >
     );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,94 @@
+import { useEffect, useState } from "react";
 import { Cards } from "../components/Cards.jsx";
 import { Link } from "react-router-dom";
 
 
 export const Home = () => {
+	const [contactList, setContactList] = useState([])
+	const [usuario, setUsuario] = useState("")
+	const [usuarioAnterior, setUsuarioAnterior] = useState("")
+	const [render, setRender] = useState("")
 
+	function getContactList() {
+		fetch('https://playground.4geeks.com/contact/agendas/' + usuario + '/contacts', { method: "GET" }) //busca info en la url
+			.then((response) => {
+				if (response.status === 404) {
+					if (confirm("El usuario " + usuario + " no extiste. ¿Quieres crearlo?")) {
+						createUser()
+					} else {
+						setUsuario(usuarioAnterior)
+					}
+				}
+				return response.json()
+
+			}) 			// promete que si llega la info la guardo en un formato, en este caso json (no es una extensión, es formato de datos) para poder interpretarla y crear una clase
+			.then((data) => Array.isArray(data.contacts) ? setContactList(data.contacts) : console.log("No hay usuario"))  	//promete que si se formatea bien lo guarda en un espacio, en este caso data, y lo procesa, en ese caso, hace un console.log
+			.catch() 			//si algo sale mal, avisa
+	}
+	function createUser() {
+		fetch('https://playground.4geeks.com/contact/agendas/' + usuario, {
+			method: "POST"
+		})
+			.then((response) => {
+				if (response.status === 201) {
+					getContactList()
+				}
+				return response.json()
+			})
+			.then()
+			.catch()
+	}
+
+	function changeUser(event) {
+		if (event.key === "Enter") {
+			setUsuarioAnterior(usuario)
+			setUsuario(event.target.value === "" ? usuario : event.target.value.toLowerCase())
+			event.target.value = ""
+		}
+	}
+
+	function deleteUser(user) {
+		fetch('https://playground.4geeks.com/contact/agendas/' + user, { method: "DELETE" })
+			.then((response) => {
+				if (confirm("¿Quieres borrar el usuario " + usuario + "?")) {
+					if (response.status === 204) {
+						setUsuario(usuarioAnterior)
+					}
+				}
+
+			})
+			.then()
+			.catch()
+
+	}
+
+	useEffect(() => {
+		//código que queremos que se ejecute al cargar el componente
+		if (usuario != "") {
+			getContactList()
+		}
+	}, [usuario])
+
+	useEffect(() => {
+		//código que queremos que se ejecute al cargar el componente
+		if (contactList != []) {
+			setRender(
+				<div className="container text-end mt-5 w-75">
+					<Cards usuario={usuario} contactList={contactList} getContactList={() => getContactList()} />
+				</div>
+			)
+		}
+	}, [contactList])
 
 	return (
-		<div className="container text-end mt-5 w-50">
-			<Link to="/add-contact"><button className="btn btn-primary">Añadir Contacto</button></Link>
-			<Cards />
+		<div className="container text-center mt-5 w-75">
+			<input type="text" className="shadow-lg rounded p-2 my-3 mx-auto" placeholder="Usuario" onKeyDown={changeUser} />
+			<h1 className="mb-3">{usuario === "" ? "Escriba el usuario de la agenda" : "Lista de contactos de " + usuario + ":"}</h1>
+			<Link to="/add-contact" className={usuario === "" ? "btn btn-info me-0 align-self-end hide" : "btn btn-info me-0 align-self-end"}>Añadir Contacto</Link>  {/* Basta con poner una clase de botón en la etiqueta link, no hace falta añadir etiqueta botón */}
+
+			{render}
+			<button className={usuario === "" ? "btn btn-danger me-0 hide align-self-end" : "btn btn-danger me-0 align-self-end"} onClick={() => deleteUser(usuario)}>Eliminar usuario</button>
+
 		</div>
 	);
 }; 
